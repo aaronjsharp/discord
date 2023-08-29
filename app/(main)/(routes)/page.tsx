14a0c4ff04e04ead7d92f5f08326b0de8e06b294
aaronjsharp <aaron.sharp2011@gmail.com>
2023-08-29 10:06:23 -0500

    added sign-up/sign-in functionality using Clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs/app-beta"
 
 export default function Home() {
   return (
     <div>
-      This is a protected route.
+      <UserButton 
+        afterSignOutUrl="/"
+      />
     </div>
     
   )
